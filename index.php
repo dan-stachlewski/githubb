8ff7969c2dff5b4f9ed4c54c9b973a0fe849efec
dan-stachlewski <dan.stachlewski@gmail.com>
2015-07-11 10:58:28 +1000

    Updating GitHub w Website data

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>This is a new GitHub Page</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/styles.css">
         <link rel="author" href="humans.txt">
     </head>
     <body>
@@ -13,12 +13,14 @@
 	<div class="page-wrapper">
 		<article>
 		<h2>Author: John Doe</h2>
+		<h3><?= date('D m Yy | h:m:s'); ?></h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
 		</article>
 		<article>
 		<h2>Author: Jane Bloggs</h2>
+		<h3><?= date('d-m-y'); ?></h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
