816472dba43fb689acb7c3b062cff2939396a2e7
dan-stachlewski <dan.stachlewski@gmail.com>
2015-07-11 10:38:45 +1000

    Added some content to the index.php file

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>This is a new GitHub Page</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+	<h1>This is a new GitHub Page</h1>
+	<div class="page-wrapper">
+		<article>
+		<h2>Author: John Doe</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+		</article>
+		<article>
+		<h2>Author: Jane Bloggs</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+		</article>
+		<article>
+		<h2>Author: Jane Doe</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+		</article>
+		<article>
+		<h2>Author: Jim Bloggs</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis nesciunt, dolorum deserunt, itaque incidunt sint ducimus inventore cum, dolorem placeat, et aspernatur corporis totam temporibus ea maxime quidem a.</p>
+		</article>
+	</div>
+        <script src="js/main.js"></script>
+    </body>
+</html>
